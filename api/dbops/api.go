8d4515c9c6da00af6a29afa67940b24f63c1093f
marshhu <marshhu@qq.com>
2019-07-28 23:50:42 +0800

    优化接口

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -1,6 +1,7 @@
 package dbops
 
 import (
+	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 )
@@ -10,8 +11,11 @@ func AddUserCredential(loginName string, pwd string) error {
 	if err != nil {
 		return err
 	}
-	stmtIns.Exec(loginName, pwd)
-	stmtIns.Close()
+	_,err = stmtIns.Exec(loginName, pwd)
+	if err != nil{
+		return  err
+	}
+	defer stmtIns.Close()
 	return nil
 }
 
@@ -22,8 +26,12 @@ func GetUserCredential(loginName string) (string, error) {
 		return "", err
 	}
 	var pwd string
-	stmtOut.QueryRow(loginName).Scan(&pwd)
-	stmtOut.Close()
+	err = stmtOut.QueryRow(loginName).Scan(&pwd)
+	if err !=nil && err != sql.ErrNoRows{
+		return "", err
+	}
+
+	defer stmtOut.Close()
 	return pwd, nil
 }
 
@@ -33,7 +41,10 @@ func DeleteUser(loginName string, pwd string) error {
 		log.Printf("delete user error:%s", err)
 		return err
 	}
-	stmtDel.Exec(loginName, pwd)
-	stmtDel.Close()
+	_,err = stmtDel.Exec(loginName, pwd)
+	if err != nil{
+		return  err
+	}
+	defer stmtDel.Close()
 	return nil
 }
